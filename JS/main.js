401b7fe48c1945921e6b5a325adc4cdf42ebb032
Jonathan Wade <jnwade@fullsail.edu>
2012-07-12 19:14:48 -0400

    Fixed store data and completed get data

--- JS/main.js
+++ JS/main.js
@@ -14,7 +14,6 @@ function showValue(newValue)
 
 //Wait until the DOM has loaded
 window.addEventListener("DOMContentLoaded", function(){
-
  	 	
  	//getElementByID Funtion
  	function $(x){
@@ -40,7 +39,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	//Find Value of selected radio button
 	function getRadioValue(){
-		var radio = $("mainForm").learn;
+		var radio = document.forms[0].learn;
 			for(var i=0; i<radio.length; i++){
 				if(radio[i].checked){
 					learnValue = radio[i].value;
@@ -64,7 +63,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		getRadioValue();
 		getCheckValue();
 		var item 				= {};
-			item.genres			= ["Genres:", $("genres").value];
+			item.genres			= ["Genre:", $("genres").value];
 			item.title			= ["Title:", $("title").value];
 			item.artist			= ["Artist:", $("artist").value];
 			item.tempo			= ["Tempo:", $("tempo").value];
@@ -76,6 +75,35 @@ window.addEventListener("DOMContentLoaded", function(){
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Song Saved!"); 		
  	}
+ 	
+ 	function getData() {
+	 	//Write data from localStorage to the Browser
+	 	var makeDiv = document.createElement("div");
+	 	makeDiv.setAttribute("id", "items");
+	 	var makeList = document.createElement("ul");
+	 	makeDiv.appendChild(makeList);
+	 	document.body.appendChild(makeDiv);
+	 	for(var i = 0, j = localStorage.length; i<j; i++){
+		 	var makeLi = document.createElement("li");
+		 	makeList.appendChild(makeLi);
+		 	var Key = localStorage.key(i);
+		 	var value = localStorage.getItem(key);
+		 	// Here we are converting our localStorage string value back into an object using JSON.parse().
+		 	var jsonObject = JSON.parse(value);
+		 	var makeSubList = document.createElement("ul");
+		 	makeLi.appendChild(makeSubList);
+		 	for(var i in jsonObject){
+			 	var makeSubLi = document.createElement("li");
+			 	makeSubList.appendChild(makeSubLi);
+			 	var dataInfo = jsonObject[i][0]+" "+jsonObject[i][1];
+			 	makeSubLi.innerHTML = dataInfo;
+		 	}
+		 	
+		 	
+	 	}
+	 	
+	 	
+ 	}
 
  	//Variable Defaults
  	var songGenre = ["Pick A Genre!", "Disco", "Funk", "Classic Rock", "80s alt", "Hair Metal", "90s Rock" ],
@@ -86,10 +114,10 @@ window.addEventListener("DOMContentLoaded", function(){
  	createGenres();
  	
  	// Set Link & Submit Click Events
- 	/*
-var viewList = $('viewList');
+ 	var viewList = $('viewList');
  	viewList.addEventListener("click", getData);
- 	var clearList = $('clearList');
+ 	/*
+var clearList = $('clearList');
  	clearList.addEventListener("click", clearLocal);
 */
  	var addSong = $("submitButton");
